3edc2e901e2d6793e78486fa3d9cf9ce0383ee50
aziziharun <aziziharun@yahoo.com>
2019-02-27 10:13:59 +0800

    new edit

--- sakila/film_svc.php
+++ sakila/film_svc.php
@@ -19,12 +19,20 @@ if (! $conn) {
 //2. query data
 $sql = "SELECT * FROM film LIMIT 0,20";
 $rs =  mysqli_query($conn,$sql);
+$arr= [];
 
 while($row = mysqli_fetch_array($rs)) {
-    echo $row['title'] . '<br>';
+    // echo $row['title'] . '<br>';
+
+    // create object dlm array utk display 2 colum
+    $obj = new stdClass();
+    $obj->title = $row['title'];
+    $obj->descr = $row['description'];
+    $arr[] = $obj;
 }
 
 
-
 //3. print data sbg json
-
+$json = json_encode($arr);
+header('Content-Type : application/json');
+echo $json;
